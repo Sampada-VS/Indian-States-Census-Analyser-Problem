87422919aaffc2f12c3eb96eb45ca1a74ab3cc18
Sampada-VS <samssv1@gmail.com>
2020-11-16 23:25:06 +0530

    Refactored code to take census and state csv data in list instead of iterating through a file

--- src/main/java/com/blz/censusanalyser/OpenCSVBuilder.java
+++ src/main/java/com/blz/censusanalyser/OpenCSVBuilder.java
@@ -2,20 +2,29 @@ package com.blz.censusanalyser;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class OpenCSVBuilder<E> implements ICSVBuilder<E> {
 	public Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuilderException {
+		return this.getCSVBean(reader, csvClass).iterator();
+	}
+
+	@Override
+	public List getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException {
+		return this.getCSVBean(reader, csvClass).parse();
+	}
+
+	private CsvToBean<E> getCSVBean(Reader reader, Class csvClass) throws CSVBuilderException {
 		try {
 			CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
 			csvToBeanBuilder.withType(csvClass);
 			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-			CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-			return csvToBean.iterator();
+			return csvToBeanBuilder.build();
 		} catch (IllegalStateException e) {
-			throw new CSVBuilderException(e.getMessage(), CSVBuilderException.ExceptionType.UNABLE_TO_PARSE);		
+			throw new CSVBuilderException(e.getMessage(), CSVBuilderException.ExceptionType.UNABLE_TO_PARSE);
 		}
 	}
 }
--- src/main/java/com/blz/censusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/blz/censusanalyser/StateCensusAnalyser.java
@@ -5,31 +5,32 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
 
 public class StateCensusAnalyser {
 	public int loadStateCensusData(String csvFilePath) throws CensusAnalyserException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
 			ICSVBuilder csvBuilder = CSVBuilderFactory.creteCSVBuilder();
-			Iterator<CSVStateCensus> censusCSVIterator = csvBuilder.getCSVFileIterator(reader, CSVStateCensus.class);
-			return getCount(censusCSVIterator);
+			List<CSVStateCensus> censusCSVList = csvBuilder.getCSVFileList(reader, CSVStateCensus.class);
+			return censusCSVList.size();
 		} catch (IOException e) {
 			throw new CensusAnalyserException(e.getMessage(),
 					CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
-		}catch(CSVBuilderException e) {
-			throw new CensusAnalyserException(e.getMessage(),e.type.name());
+		} catch (CSVBuilderException e) {
+			throw new CensusAnalyserException(e.getMessage(), e.type.name());
 		}
 	}
 
 	public int loadStateCodeData(String csvFilePath) throws CensusAnalyserException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
 			ICSVBuilder csvBuilder = CSVBuilderFactory.creteCSVBuilder();
-			Iterator<CSVStateCode> codeCSVIterator = csvBuilder.getCSVFileIterator(reader, CSVStateCode.class);
-			return getCount(codeCSVIterator);
+			List<CSVStateCode> codeCSVList = csvBuilder.getCSVFileList(reader, CSVStateCode.class);
+			return codeCSVList.size();
 		} catch (IOException e) {
 			throw new CensusAnalyserException(e.getMessage(),
 					CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
 		} catch (CSVBuilderException e) {
-			throw new CensusAnalyserException(e.getMessage(),e.type.name());
+			throw new CensusAnalyserException(e.getMessage(), e.type.name());
 		}
 	}
 
